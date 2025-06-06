be5746a4c84197557fc6cd00f87233be5254431a
gaowenhan <1049912732@qq.com>
2025-06-06 14:50:11 +0800

    Fix template path resolution

--- bin/create.js
+++ bin/create.js
@@ -5,13 +5,16 @@ import { execSync } from 'child_process';
 
 console.log('Vue 3 + TypeScript项目模板创建工具');
 
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = path.dirname(__filename);
+
 const projectName = process.argv[2];
 if (!projectName) {
   console.error('请指定项目名称: pnpm create gaowenhan-vtw <project-name>');
   process.exit(1);
 }
 
-const templateDir = path.join(process.cwd(), 'template');
+const templateDir = path.join(__dirname, '../template');
 const projectDir = path.join(process.cwd(), projectName);
 
 // 创建项目目录
